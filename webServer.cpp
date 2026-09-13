4617fe3acc9e8a3a1bb981a6a59da42192b4a187
cadennubel <cadennubel@gmail.com>
2026-09-12 19:46:03 -0600

    Update webServer.cpp

--- webServer.cpp
+++ webServer.cpp
@@ -4,6 +4,12 @@
 // *************************************************************************
 // * processConnect()
 // *************************************************************************
+void sig_handler(int signo) {
+ DEBUG << "Caught signal #" << signo << ENDL;
+ DEBUG << "Closing file descriptors 3-31." << ENDL;
+ closefrom(3);
+ exit(1);
+}
 int processConnection(int sockFd) {
   std::string filename, request, bodyBegin;
   size_t contentLength = 0;
